c72e739558216ac4391b00cfccf1a3954e68bcbe
pablochenko <pablo.chenko10@gmail.com>
2022-09-02 14:50:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ res.download(file);
  })
 
 app.get('/', (req, res) => {
-res.send("servidor funcionando..2.")
+res.send("servidor funcionando..21.")
 /*
   res.set({
     'Content-Disposition' : 'attachment; filename=demo.pdf',
