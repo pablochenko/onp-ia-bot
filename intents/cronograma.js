c536dd1965737fd923aa2043db26909ce6b36e33
cuentademodev <cuentademodev@gmail.com>
2022-09-19 16:06:32 -0500

    update

--- intents/cronograma.js
+++ intents/cronograma.js
@@ -36,26 +36,27 @@ function payload_opciones(){
   function cronograma_info(agent){        
     const cronograma_mes=agent.parameters.cronograma_mes;  
     let opciones = payload_opciones();
-   /// const contexto = '4cronogramadepagos-followup'; //MINUSCULAS 
-   //const parametros = {'cron': 1};
+    let list_opc = [];
+    list_opc.push('🗓<u><b>Cronograma de pagos '+cronograma_mes+' '+ d.getFullYear()+':</b></u>\n'); 
 
-
-    let mensaje = '🗓<u><b>Cronograma de pagos '+cronograma_mes+' '+ d.getFullYear()+':</b></u>\n'; 
     for (const cron of v_cronograma) { 
       if(cronograma_mes == cron.mes_desc){
-        mensaje += "\n<i><b>"+cron.tipo_1+"</b></i>\n"+cron.tipo_1_desc+"\n---------- ---------- ----------";
-        mensaje += "\n<i><b>"+cron.tipo_2+"</b></i>\n"+cron.tipo_2_desc+"\n---------- ---------- ----------";
-        mensaje += "\n<i><b>"+cron.tipo_3+"</b></i>\n"+cron.tipo_3_desc+"\n---------- ---------- ----------";
-        mensaje += "\n<i><b>"+cron.tipo_4+"</b></i>\n"+cron.tipo_4_desc+"\n---------- ---------- ----------";    
-        mensaje += "\n<i><b>"+cron.tipo_5+"</b></i>\n"+cron.tipo_5_desc;   
+        list_opc.push("\n<i><b>"+cron.tipo_1+"</b></i>\n"+cron.tipo_1_desc);
+        list_opc.push("\n<i><b>"+cron.tipo_2+"</b></i>\n"+cron.tipo_2_desc);
+        list_opc.push("\n<i><b>"+cron.tipo_3+"</b></i>\n"+cron.tipo_3_desc);
+        list_opc.push("\n<i><b>"+cron.tipo_4+"</b></i>\n"+cron.tipo_4_desc);
+        list_opc.push("\n<i><b>"+cron.tipo_5+"</b></i>\n"+cron.tipo_5_desc);
       }       
     }           
-    const payload = { "telegram": {"text": mensaje,
-                      "reply_markup": {
+    const payload = {"telegram": {"text": "Por favor selecciona una opción 👇",
+                     "reply_markup": {
                         "inline_keyboard": opciones
                         },
-                      "parse_mode": "HTML"}};   
-    agent.add(new Payload(agent.UNSPECIFIED , payload, {rawPayload: true, sendAsMessage: true}));
+                      "parse_mode": "HTML"}};                  
+    list_opc(new Payload(agent.UNSPECIFIED , payload, {rawPayload: true, sendAsMessage: true}));
+    
+    
+    agent.add(list_opc);   
    // agent.context.set({ name: contexto, lifespan: 2, parameters: parametros });    
     
   };     
